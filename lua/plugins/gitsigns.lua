3cb52d04d827434f699a4f5e0b64d97ad0efbfd4
Caleb Corpening <caleb.roosevelt.corpening@gmail.com>
2025-11-10 10:16:00 -0500

    render markdown and vim wiki

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -44,15 +44,15 @@ return {
         gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
       end, { desc = 'git [r]eset hunk' })
       -- normal mode
-      map('n', '<leader>gs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
-      map('n', '<leader>gr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
-      map('n', '<leader>gS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
-      map('n', '<leader>gu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
-      map('n', '<leader>gR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
-      map('n', '<leader>gp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
-      map('n', '<leader>gb', gitsigns.blame_line, { desc = 'git [b]lame line' })
-      map('n', '<leader>gd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
-      map('n', '<leader>gD', function()
+      map('n', '<leader>ggs', gitsigns.stage_hunk, { desc = 'git [s]tage hunk' })
+      map('n', '<leader>ggr', gitsigns.reset_hunk, { desc = 'git [r]eset hunk' })
+      map('n', '<leader>ggS', gitsigns.stage_buffer, { desc = 'git [S]tage buffer' })
+      map('n', '<leader>ggu', gitsigns.stage_hunk, { desc = 'git [u]ndo stage hunk' })
+      map('n', '<leader>ggR', gitsigns.reset_buffer, { desc = 'git [R]eset buffer' })
+      map('n', '<leader>ggp', gitsigns.preview_hunk, { desc = 'git [p]review hunk' })
+      map('n', '<leader>ggb', gitsigns.blame_line, { desc = 'git [b]lame line' })
+      map('n', '<leader>ggd', gitsigns.diffthis, { desc = 'git [d]iff against index' })
+      map('n', '<leader>ggD', function()
         gitsigns.diffthis '@'
       end, { desc = 'git [D]iff against last commit' })
       -- Toggles
