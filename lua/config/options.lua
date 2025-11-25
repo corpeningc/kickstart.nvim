dabeb95af0b9d2aa68dd508e80e6dd5dbb329558
Caleb Corpening <caleb.roosevelt.corpening@gmail.com>
2025-11-25 14:35:53 -0500

    block cursor

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -21,6 +21,8 @@ vim.o.number = true
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
 
+vim.opt.guicursor = 'n-v-i-c:block-Cursor'
+
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
