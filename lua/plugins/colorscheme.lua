a062e707a763cf261f08e6b2f685471208195b4b
Caleb Corpening <caleb.roosevelt.corpening@gmail.com>
2025-12-09 13:52:50 -0500

    bleck color

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,6 +4,18 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require('kanagawa').setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg = '#1c1917', -- oklch(27.4% 0.006 286.033)
+                bg_gutter = 'none',
+              },
+            },
+          },
+        },
+      }
       vim.cmd [[colorscheme kanagawa]]
     end,
   },
